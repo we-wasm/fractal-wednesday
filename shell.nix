0efa3b1ef1478adebf279c7cf65a4f798a6b7243
jared-w <jaredweakly@gmail.com>
2019-12-22 08:18:05 -0800

    Basic nix-shell support for rust otolchain

---
+++ shell.nix
@@ -0,0 +1,14 @@
+# $ cargo-web build --target=wasm32-unknown-unknown
+# $ cargo-web start --target=wasm32-unknown-unknown
+# $ cargo-web deploy --target=wasm32-unknown-unknown
+{ pkgs ? import ./nix { }, channel ? "nightly", date ? "2019-12-21", ... }:
+let
+  rPkg = pkgs.rustChannelOf { inherit date channel; };
+  rWasm = rPkg.rust.override { targets = [ "wasm32-unknown-unknown" ]; };
+
+in pkgs.mkShell {
+  buildInputs = with pkgs; [ cargo-web wasm-pack wabt rWasm ];
+
+  # This is for Nix/NixOS compatibility with RLS/rust-analyzer
+  RUST_SRC_PATH = "${rPkg.rust-src}/lib/rustlib/src/rust/src";
+}
