ad9f048b1e32c236cda16ee2914c3d974a6f421e
jared-w <jaredweakly@gmail.com>
2019-12-29 11:22:09 -0800

    Fix run script execution

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+name="$1"
+shift
 
 # Usage: ./run.sh bare-metal-wasm
-$(nix-build ../default.nix --no-out-link -A makeWeb --argstr name "$1" "$@")
+$(nix-build "$parent_path/default.nix" --no-out-link \
+    -A makeWeb \
+    --argstr name "$name" \
+    "$@" \
+)
